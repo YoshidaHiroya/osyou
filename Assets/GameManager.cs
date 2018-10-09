951b31cab7536ed7d9d72d589668c258029f49d3
YoshidaHiroya <hdotm1225@gmail.com>
2018-10-09 20:59:19 +0900

    firstcommit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour {
+	
+	public int MAX_ORB = 10;
+
+	public GameObject orbPrefab;
+
+	public GameObject canvasGame;
+
+	public GameObject textScore;
+
+	int score=0;
+	int nextscore=100;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < MAX_ORB; i++) {
+			CreateOrb ();
+		}
+		RefreshScoreText ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void CreateOrb(){
+		GameObject orb = (GameObject)Instantiate (orbPrefab);
+		orb.transform.SetParent (canvasGame.transform,false);
+		orb.transform.position = new Vector3 (Random.Range(200,300),Random.Range(500,600),0);
+
+	}
+	public void GetOrb(){
+		score += 1;
+		RefreshScoreText ();
+}
+	void RefreshScoreText(){
+		textScore.GetComponent<Text> ().text =
+			"徳:" + score + "/" + nextscore;
+	}
+}
